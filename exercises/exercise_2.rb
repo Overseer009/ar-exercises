a13b2e25cca4e50ff82cceecab0bae825cacaf22
Overseer009 <sacha.sunnasy@gmail.com>
2021-06-21 19:02:05 -0400

    complete the second exercise

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+puts @store1 = Store.find_by(id: 1)
+
+puts @store2 = Store.second
+
+@store1.name = "The Mall"
+@store1.save
+
+puts @store1.name
