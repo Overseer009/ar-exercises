f6009530858e430ce769d59656a9827a91a80f90
Overseer009 <sacha.sunnasy@gmail.com>
2021-06-21 19:44:32 -0400

    complete the fifth exercise

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_ann_rev = Store.sum(:annual_revenue)
+puts @total_ann_rev
+
+@ave_ann_rev = Store.average(:annual_revenue)
+puts @ave_ann_rev
+
+puts Store.where("annual_revenue > 1000000").count
